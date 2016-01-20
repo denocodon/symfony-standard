ae1acb307f5fa362bdb35d849fb5defe3b8891d9
denocodon <itrungnguyenthanh@gmail.com>
2016-01-20 17:23:04 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 echo "hello" > mo.txt
 export SYMFONY_ENV=prod
 composer install --no-dev --optimize-autoloader
-php app/console cache:clear --env=prod --no-debug
-php app/console assets:install web_directory
-php app/console assetic:dump web_directory
+php bin/console cache:clear --env=prod --no-debug
+php bin/console assets:install web_directory
+php bin/console assetic:dump web_directory
 mkdir -p builds/logs
 mkdir -p builds/$BUILD_NUMBER/cloverphp
 mkdir -p builds/api
