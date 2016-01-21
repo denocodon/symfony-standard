cb494f364c73ebeaa5f86fe075421f92d1aa17ed
denocodon <itrungnguyenthanh@gmail.com>
2016-01-21 13:53:02 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ php bin/console assets:install --env=prod
 mkdir -p builds/logs
 
 phpunit --coverage-clover ./builds/logs/clover.xml
-phpmd . xml codesize,unusedcode,naming,design --reportfile builds/logs/phppmd.xml --exclude Vendor,Config,Console,uploads,tmp,Test
+#phpmd . xml codesize,unusedcode,naming,design --reportfile builds/logs/phppmd.xml --exclude Vendor,Config,Console,uploads,tmp,Test
+phploc . --log-xml=builds/logs/phploc.xml
 #mkdir -p builds/$BUILD_NUMBER/cloverphp
 #mkdir -p builds/api
 
