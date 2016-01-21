9350891158ce1f72bf8e65f3599ca1d17ba6d28e
denocodon <itrungnguyenthanh@gmail.com>
2016-01-21 11:54:26 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-composer install --no-dev --optimize-autoloader
+export SYMFONY_ENV=prod
+composer install  --optimize-autoloader
 php bin/console cache:clear --env=prod --no-debug
 php bin/console assets:install --env=prod
 #php bin/console assets:install web_directory
 #php bin/console assetic:dump web_directory
 mkdir -p builds/logs
-export SYMFONY_ENV=prod
+
 phpunit --coverage-clover ./builds/logs/clover.xml
 #mkdir -p builds/$BUILD_NUMBER/cloverphp
 #mkdir -p builds/api
