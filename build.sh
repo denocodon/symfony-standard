02aecd67504b3a32b4a83dda2e2063a75d582bbc
denocodon <itrungnguyenthanh@gmail.com>
2016-01-21 11:43:31 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ php bin/console cache:clear --env=prod --no-debug
 php bin/console assets:install --env=prod
 #php bin/console assets:install web_directory
 #php bin/console assetic:dump web_directory
-#mkdir -p builds/logs
+mkdir -p builds/logs
+phpunit --coverage-clover 'builds/logs/clover.xml
 #mkdir -p builds/$BUILD_NUMBER/cloverphp
 #mkdir -p builds/api
 
