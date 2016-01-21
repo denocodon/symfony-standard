2253e92c0693ff2a1003c9ab9e7c27b233ac5255
denocodon <itrungnguyenthanh@gmail.com>
2016-01-21 11:37:02 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ composer install --no-dev --optimize-autoloader
 php bin/console cache:clear --env=prod --no-debug
 php bin/console assets:install web_directory
 php bin/console assetic:dump web_directory
-mkdir -p builds/logs
-mkdir -p builds/$BUILD_NUMBER/cloverphp
-mkdir -p builds/api
+#mkdir -p builds/logs
+#mkdir -p builds/$BUILD_NUMBER/cloverphp
+#mkdir -p builds/api
 
