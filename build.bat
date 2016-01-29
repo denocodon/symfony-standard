35f615625e676f1e7e8bb4e1c0d9590b2a116c75
denocodon <itrungnguyenthanh@gmail.com>
2016-01-29 12:57:45 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,2 @@
 SET SYMFONY_ENV=prod
-del /f vendor/*
-php bin/console cache:clear --env=prod --no-debug
-php bin/console assets:install --env=prod
-composer install  --optimize-autoloader
+ composer install --no-dev --optimize-autoloader
