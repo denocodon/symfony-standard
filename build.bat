1c1ec43f30a37f97fb9d9adbc0a048e1295de1d0
denocodon <itrungnguyenthanh@gmail.com>
2016-01-29 14:02:15 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 SET SYMFONY_ENV=prod
 composer install
-mkdir builds/logs/$BUILD_NUMBER
-phpunit --log-junit builds/logs/$BUILD_NUMBER/phpunit.xml
+mkdir builds/logs/%BUILD_NUMBER%
+phpunit --log-junit builds/logs/%BUILD_NUMBER%/phpunit.xml
