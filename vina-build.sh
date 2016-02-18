4d4d7b13cdf2a9495efb3dd3708328b89bef3aee
denocodon <itrungnguyenthanh@gmail.com>
2016-02-18 10:31:53 +0700

    Create vina-build.sh

---
+++ vina-build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+export SYMFONY_ENV=prod
+composer install  --optimize-autoloader
+php bin/console cache:clear --env=prod --no-debug
+php bin/console assets:install --env=prod
+#php bin/console assets:install web_directory
+#php bin/console assetic:dump web_directory
+#mkdir -p builds/logs
+
+#phpcs --report=checkstyle --report-file=builds/logs/phpcs.xml .
+#phpunit --coverage-clover ./builds/logs/clover.xml
+phpunit --log-junit phpunit.xml
+#phpmd . xml codesize,unusedcode,naming,design --reportfile builds/logs/phppmd.xml --exclude Vendor,Config,Console,uploads,tmp,Test
+#phploc . --log-xml=builds/logs/phploc.xml
+#phploc . --log-csv=builds/logs/phploc.csv
+#mkdir -p builds/$BUILD_NUMBER/cloverphp
+#mkdir -p builds/api
+
